67d5f5e40b8bd159935de57248eeceed9a6fff28
Kevin Tran <kevin.tran@leonardo.com>
2018-06-11 16:49:44 -0400

    Just rename a property

--- grpc-client/src/main/java/org/tnmk/skeleton/springgrpc/client/rest/ItemController.java
+++ grpc-client/src/main/java/org/tnmk/skeleton/springgrpc/client/rest/ItemController.java
@@ -4,25 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.tnmk.practice.springgrpc.protobuf.ItemProto;
-import org.tnmk.practice.springgrpc.protobuf.ItemIdProto;
 import org.tnmk.skeleton.springgrpc.client.grpcclient.Item;
 import org.tnmk.skeleton.springgrpc.client.grpcclient.ItemId;
 import org.tnmk.skeleton.springgrpc.client.grpcclient.ItemProtoClient;
 
 @RestController
 public class ItemController {
-    private final ItemProtoClient itemGrpcClient;
+    private final ItemProtoClient itemProtoClient;
 
     @Autowired
-    public ItemController(ItemProtoClient itemGrpcClient) {
-        this.itemGrpcClient = itemGrpcClient;
+    public ItemController(ItemProtoClient itemProtoClient) {
+        this.itemProtoClient = itemProtoClient;
     }
 
     @RequestMapping("/items/{id}")
     public Item getItems(@PathVariable("id") String id) {
         ItemId itemId = new ItemId(); itemId.setId(id);
-        Item item = itemGrpcClient.getItem(itemId);
+        Item item = itemProtoClient.getItem(itemId);
         return item;
     }
 
