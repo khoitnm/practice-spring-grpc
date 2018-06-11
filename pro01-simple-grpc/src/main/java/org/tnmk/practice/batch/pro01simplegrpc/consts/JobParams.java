bd815112652500dc4bac7f0cbf008460d4664064
Kevin Tran <kevin.tran@leonardo.com>
2018-06-11 12:49:02 -0400

    Switch from gradle to maven

--- pro01-simple-grpc/src/main/java/org/tnmk/practice/batch/pro01simplegrpc/consts/JobParams.java
+++
@@ -1,9 +0,0 @@
-package org.tnmk.practice.batch.pro01simplegrpc.consts;
-
-public class JobParams {
-    public static final String PARAM_INPUT_FILE_PATH = "inputFilePath";
-    public static final String PARAM_CHUNK_SIZE = "chunkSize";
-    public static final String PARAM_THREADS_COUNT= "threadsCount";
-
-
-}
--- pro01-simple-grpc/src/main/java/org/tnmk/practice/batch/pro01simplegrpc/job/step/StepContextItems.java
+++
@@ -1,5 +0,0 @@
-package org.tnmk.practice.batch.pro01simplegrpc.job.step;
-
-public class StepContextItems {
-    public static final String STEP_KEY_ITEMS = "items";
-}
