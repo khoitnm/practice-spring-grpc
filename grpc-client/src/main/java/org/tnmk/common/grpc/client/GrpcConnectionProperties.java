6385b30562bc10f0275f9c622e4d00b5ce5b5e75
Khoi (Kevin) Tran <khoi.tnm@gmail.com>
2018-09-28 18:10:39 -0400

    Restructure the whole project

---
+++ grpc-client/src/main/java/org/tnmk/common/grpc/client/GrpcConnectionProperties.java
@@ -0,0 +1,22 @@
+package org.tnmk.common.grpc.client;
+
+public class GrpcConnectionProperties {
+    private String host;
+    private int port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
---
+++ pro02-error-handler/src/main/java/org/tnmk/common/grpc/global/MDCConstants.java
@@ -0,0 +1,5 @@
+package org.tnmk.common.grpc.global;
+
+public class MDCConstants {
+    public static final String CORRELATION_ID = "correlationId";
+}
