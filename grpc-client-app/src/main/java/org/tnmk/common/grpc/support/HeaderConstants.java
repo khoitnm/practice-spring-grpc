afbe822d4ed70e7d788c6b0f9c7a393baee4f968
khoi <khoi.tnm@gmail.com>
2019-07-27 12:22:52 -0400

    Clone grpc-client-app

---
+++ grpc-client-app/src/main/java/org/tnmk/common/grpc/support/HeaderConstants.java
@@ -0,0 +1,5 @@
+package org.tnmk.common.grpc.support;
+
+public class HeaderConstants {
+    public static final String CORRELATION_ID = "correlation-id";
+}
---
+++ grpc-client-app/src/main/java/org/tnmk/practice/springgrpc/client/samplestory/grpcclientapp/ItemId.java
@@ -0,0 +1,13 @@
+package org.tnmk.practice.springgrpc.client.samplestory.grpcclientapp;
+
+public class ItemId {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
