80d3ac2c345d2f0d23163f685d364a0e0b1edbef
Kevin Tran <kevin.tran@leonardo.com>
2019-08-02 14:41:39 -0400

    Forward MDC context to the subsequence thread

--- pro-04-server-interceptor-multi-thread/src/main/java/org/tnmk/common/grpc/global/GlobalGrpcServerInterceptor.java
+++ pro-04-server-interceptor-multi-thread/src/main/java/org/tnmk/common/grpc/global/GlobalGrpcServerInterceptor.java
@@ -1,13 +1,16 @@
 package org.tnmk.common.grpc.global;
 
 import io.grpc.*;
+import io.grpc.stub.MetadataUtils;
 import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.tnmk.common.grpc.global.MDCConstants.CORRELATION_ID;
@@ -16,7 +19,7 @@ import static org.tnmk.common.grpc.global.MDCConstants.CORRELATION_ID;
  * This class is processed before calling ProtoBuf Resource layer.
  * Note: To make this class works in UnitTest, you must register ServerInterceptor into {@link GrpcServerRule} or {@link GrpcCleanupRule}.
  * Please view more at {@link SampleItemGrpcServiceTest}.
- *
+ * <p>
  * You can use AOP ({@link GlobalGrpcAdvice} to handle all exception) without needing this class.<br/>
  * However, this class helps you to get headers data, which could be very useful in some cases.
  */
@@ -30,10 +33,15 @@ public class GlobalGrpcServerInterceptor implements ServerInterceptor {
     public <I, O> ServerCall.Listener<I> interceptCall(ServerCall<I, O> call, Metadata headers, ServerCallHandler<I, O> serverCallHandler) {
         try {
             String correlationId = UUID.randomUUID().toString();
+            // If we just use MDC.put(...) here, in grpcService layer, we cannot get data from MDC because they are on different threads.
+            // https://github.com/grpc/grpc-java/issues/2280
+            // That's why we need to copy the MDC's contextMap to grpcService's thread.
             MDC.put(CORRELATION_ID, correlationId);
+
             logger.info("GrpcInterceptor. newCorrelationId: {}", correlationId);
-            ServerCall.Listener<I> listener = serverCallHandler.startCall(call, headers);
-            return listener;
+            ServerCall.Listener<I> originalListener = serverCallHandler.startCall(call, headers);
+            ServerCall.Listener<I> forwardListener = createForwardContextListener(originalListener, MDC.getCopyOfContextMap());
+            return forwardListener;
         } catch (Exception ex) {
             /**
              * Actually, this catch cannot catch any exception thrown by GrpcService layer because it's not triggered yet.
@@ -45,5 +53,39 @@ public class GlobalGrpcServerInterceptor implements ServerInterceptor {
             call.close(status, headers);
             return NOOP_LISTENER;
         }
+    }
+
+    private <ReqT> ServerCall.Listener<ReqT> createForwardContextListener(ServerCall.Listener<ReqT> original, Map<String, String> mdcContext) {
+        return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(original) {
+            @Override
+            public void onMessage(final ReqT message) {
+                MDC.setContextMap(mdcContext);
+                super.onMessage(message);
+            }
+
+            @Override
+            public void onHalfClose() {
+                MDC.setContextMap(mdcContext);
+                super.onHalfClose();
+            }
+
+            @Override
+            public void onCancel() {
+                MDC.setContextMap(mdcContext);
+                super.onCancel();
+            }
+
+            @Override
+            public void onComplete() {
+                MDC.setContextMap(mdcContext);
+                super.onComplete();
+            }
+
+            @Override
+            public void onReady() {
+                MDC.setContextMap(mdcContext);
+                super.onReady();
+            }
+        };
     }
 }
