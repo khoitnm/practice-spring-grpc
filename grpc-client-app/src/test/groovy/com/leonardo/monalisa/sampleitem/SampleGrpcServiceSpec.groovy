d683ebcc73625f4a4d41cef90d56ace94a4386af
Kevin Tran <kevin.tran@leonardo.com>
2019-08-07 16:21:40 -0400

    Use grpc-client-app to connect TLS Server. The TLS Client has some issue which cannot connect via TLS gRPC

---
+++ grpc-client-app/src/test/groovy/com/leonardo/monalisa/sampleitem/SampleGrpcServiceSpec.groovy
@@ -0,0 +1,45 @@
+package com.leonardo.monalisa.sampleitem
+
+import com.leonardo.monalisa.BaseSpecification
+import com.leonardo.monalisa.hotelmanagement.hotelview.proto.HotelViewGrpcServiceGrpc
+import com.leonardo.monalisa.hotelmanagement.hotelview.proto.HotelViewProto
+import com.leonardo.monalisa.hotelmanagement.hotelview.proto.HotelViewRequestProto
+import org.springframework.beans.factory.annotation.Autowired
+import org.tnmk.practice.springgrpc.grpcclientapp.config.GrpcClientStubFactory
+import org.tnmk.practice.springgrpc.protobuf.ItemIdProto
+import org.tnmk.practice.springgrpc.protobuf.ItemProto
+import org.tnmk.practice.springgrpc.protobuf.SampleItemGrpcServiceGrpc
+
+class SampleGrpcServiceSpec extends BaseSpecification {
+
+    @Autowired
+    private GrpcClientStubFactory grpcClientStubFactory;
+
+    private SampleItemGrpcServiceGrpc.SampleItemGrpcServiceBlockingStub stub;
+
+    /**
+     * run before the first feature method
+     * @return
+     */
+    def setup(){
+        stub = grpcClientStubFactory.constructStub(
+                "sample-item",
+                SampleItemGrpcServiceGrpc.SampleItemGrpcServiceBlockingStub.class);
+    }
+
+
+    def 'Verify find content by Hotelview and Oid account'() {
+        given:
+
+        when:
+        ItemIdProto idProto = ItemIdProto.newBuilder().setId(""+System.nanoTime()).build();
+
+        ItemProto itemProto = stub.getItem(idProto)
+
+        System.out.println(itemProto)
+
+        then:
+        noExceptionThrown()
+    }
+
+}
