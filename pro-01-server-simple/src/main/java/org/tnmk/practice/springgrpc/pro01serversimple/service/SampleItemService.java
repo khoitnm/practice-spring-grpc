f4b1b68aac96664b6baed99c82cc7555f38606db
Kevin Tran <kevin.tran@leonardo.com>
2019-10-08 11:37:30 -0400

    Update simple server: add log & add java 8 properties.

--- pro-01-server-simple/src/main/java/org/tnmk/practice/springgrpc/pro01serversimple/service/SampleItemService.java
+++ pro-01-server-simple/src/main/java/org/tnmk/practice/springgrpc/pro01serversimple/service/SampleItemService.java
@@ -1,16 +1,22 @@
 package org.tnmk.practice.springgrpc.pro01serversimple.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.tnmk.practice.springgrpc.protobuf.ItemProto;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Date;
 
 @Service
 public class SampleItemService {
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public ItemProto getItem(String id) {
+        logger.info("GetItem - request: "+id);
         ItemProto itemProto = ItemProto.newBuilder()
             .setId(id)
             .setName("Some name " + new Date()).build();
+        logger.info("GetItem - result: \n"+itemProto);
         return itemProto;
     }
 }
