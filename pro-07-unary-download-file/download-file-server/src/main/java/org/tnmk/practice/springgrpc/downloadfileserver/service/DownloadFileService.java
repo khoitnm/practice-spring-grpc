8bd774693e5ed455045baac372eaa0e6d28f27f6
Kevin Tran <kevin.tran@leonardo.com>
2019-11-21 16:27:04 -0500

    pro-07 generate bytes data from text.

--- pro-07-unary-download-file/download-file-server/src/main/java/org/tnmk/practice/springgrpc/downloadfileserver/service/DownloadFileService.java
+++ pro-07-unary-download-file/download-file-server/src/main/java/org/tnmk/practice/springgrpc/downloadfileserver/service/DownloadFileService.java
@@ -7,13 +7,26 @@ import org.tnmk.common.utils.IOUtils;
 
 import java.lang.invoke.MethodHandles;
 
+import static org.apache.commons.io.Charsets.UTF_8;
+
 @Service
 public class DownloadFileService {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public static final String FILE_PATH = "/samplefiles/SampleFile.txt";
 
     public byte[] getFileData(String fileName) {
-        logger.info("You are downloading the file "+fileName+"? Sorry, I only have this file '"+FILE_PATH+"'");
-        return IOUtils.loadBinaryFileInClassPath(FILE_PATH);
+        logger.info("You are downloading the file " + fileName + "? Sorry, I only have this file '" + FILE_PATH + "'");
+        //return IOUtils.loadBinaryFileInClassPath(FILE_PATH);
+        String fileContent = generateFileString();
+        return generateFileDataFromText(fileContent);
+    }
+
+    private String generateFileString(){
+        String fileContent = "title; I called just to say I love you";
+        return fileContent;
+    }
+
+    private byte[] generateFileDataFromText(String text) {
+        return text.getBytes(UTF_8);
     }
 }
