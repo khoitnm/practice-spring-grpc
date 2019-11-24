eb1bdb7dd878c66c3922933efff805f71d99b23c
khoi <khoi.tnm@gmail.com>
2019-11-23 21:38:14 -0500

    ro-09-stream-download-zip-files: update proto request.

---
+++ pro-09-stream-download-zip-files/README.md
@@ -0,0 +1,12 @@
+# Introduction
+This example shows how to download file with data *STREAM*.
+This is only used when you don't download a small file.
+If you download a big file, you should use data stream.
+
+# Run
+1. Start the server `stream-download-zip-files-server`
+2. Start the client `stream-download-zip-files-client`, 
+then you'll see in the log the content of the file which was downloaded from the server application.
+
+# Reference
+https://grpc.io/docs/tutorials/basic/java/
--- pro-09-stream-download-zip-files/stream-download-zip-files-server/src/main/java/org/tnmk/practice/springgrpc/streamdownloadzipfilesserver/service/StreamDownloadZipFilesService.java
+++ pro-09-stream-download-zip-files/stream-download-zip-files-server/src/main/java/org/tnmk/practice/springgrpc/streamdownloadzipfilesserver/service/StreamDownloadZipFilesService.java
@@ -1,5 +1,9 @@
 package org.tnmk.practice.springgrpc.streamdownloadzipfilesserver.service;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,20 +11,34 @@ import org.springframework.util.StreamUtils;
 import org.tnmk.common.utils.IOUtils;
 import org.tnmk.common.utils.UnexpectedException;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PipedOutputStream;
 import java.lang.invoke.MethodHandles;
 
 @Service
 public class StreamDownloadZipFilesService {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public static final String FILE_PATH = "/samplefiles/SampleFile.txt";
+    private static final int PIPE_BUFFER_SIZE = 16 * 1024;
 
     public InputStream downloadZipFile(long numZipEntries) {
+        //TODO try to understand what is Thread interruption
+        // Thread.currentThread().isInterrupted()
+
         logger.info("You are downloading a zip file with {} entries", numZipEntries);
-        InputStream inputStream = IOUtils.validateExistInputStreamFromClassPath(FILE_PATH);
-        return inputStream;
+        PipedOutputStream pipedOutputStreamForZipFile = new PipedOutputStream();
+        PipedInputStream pipedInputStreamForZipFile = new PipedInputStream(pipedOutputStreamForZipFile, PIPE_BUFFER_SIZE);
+//        ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(pipedOutputStream, PIPE_BUFFER_SIZE));
+        //It looks like using BufferOutputStream for PipedOutputStream is redundant?
+        ZipOutputStream zipOutputStream = new ZipOutputStream(pipedOutputStreamForZipFile);
+        zipOutputStream.setMethod(ZipOutputStream.DEFLATED);
+        for (int i = 0; i < numZipEntries; i++) {
+            ZipEntry zipEntry = new ZipEntry(FILE_PATH);
+            zipOutputStream.putNextEntry(zipEntry);
+            InputStream zipEntryInputStream = IOUtils.validateExistInputStreamFromClassPath(FILE_PATH);
+            StreamUtils.copy(zipEntryInputStream, zipOutputStream);
+            zipEntryInputStream.close();
+        }
+
+
+        return pipedInputStreamForZipFile;
     }
 }
