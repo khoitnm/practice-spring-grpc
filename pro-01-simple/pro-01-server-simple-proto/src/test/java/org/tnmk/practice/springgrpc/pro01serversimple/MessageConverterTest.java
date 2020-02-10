0b9b22e9a58bd5dba3b56eeac774a83cdaf585b1
Kevin Tran <kevin.tran@leonardo.com>
2020-02-10 14:27:11 -0500

    Add testing when transforming message with different field indexes.

--- pro-01-simple/pro-01-server-simple-proto/src/test/java/org/tnmk/practice/springgrpc/pro01serversimple/MessageConverterTest.java
+++ pro-01-simple/pro-01-server-simple-proto/src/test/java/org/tnmk/practice/springgrpc/pro01serversimple/MessageConverterTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.tnmk.practice.springgrpc.protobuf.MessagAChild;
 import org.tnmk.practice.springgrpc.protobuf.MessageA;
 import org.tnmk.practice.springgrpc.protobuf.MessageB;
+import org.tnmk.practice.springgrpc.protobuf.MessageC;
 
 public class MessageConverterTest {
 
@@ -29,6 +30,36 @@ public class MessageConverterTest {
         Assertions.assertEquals(messageA.getName(), messageB.getName());
         Assertions.assertEquals((int)messageA.getIdLong(), messageB.getIdInt());
         Assertions.assertEquals(messageA.getChildA().getName(), messageB.getChildB().getName());
+    }
+
+    @Test
+    public void testDataCanNOTBeTransferredWhenChangingFieldName() {
+
+        MessageA messageA = MessageA.newBuilder()
+            .setActiveA(true)
+            .setIdLong(System.nanoTime())
+            .setName("MessageAName" + System.nanoTime())
+            .setChildA(MessagAChild.newBuilder().setName("ChildA"+System.nanoTime()).build())
+            .build();
+
+
+        byte[] bytes = messageA.toByteArray();
+        MessageC messageC = parseMessageCFromBytes(bytes);
+
+        //Check whether data from MessageA should be able to converted to MessageB because the have the same field order.
+        Assertions.assertNotEquals(messageA.getActiveA(), messageC.getActiveA());
+        Assertions.assertNotEquals(messageA.getName(), messageC.getName());
+        Assertions.assertNotEquals((int)messageA.getIdLong(), messageC.getIdLong());
+        Assertions.assertNotEquals(messageA.getChildA().getName(), messageC.getChildA().getName());
+    }
+
+    private MessageC parseMessageCFromBytes(byte[] bytes) {
+        try {
+            MessageC message = MessageC.parseFrom(bytes);
+            return message;
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException("Cannot convert bytes to MessageC: " + e.getMessage(), e);
+        }
     }
 
     private MessageB parseMessageBFromBytes(byte[] bytes) {
